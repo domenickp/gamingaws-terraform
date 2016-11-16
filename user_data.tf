bcda732e898f57b314e80ac144af1e867f953d3b
Domenick Petrella <dpetrella@palantir.com>
2016-11-15 23:31:01 -0500

    Cleaning up user_data.tf

--- user_data.tf
+++ user_data.tf
@@ -1,9 +1,3 @@
-/****************************************************
-Pass-in configuration variables to AMI bootstrapping
-*****************************************************/
-
-# This doesn't seem to converge without being a template file
-# https://support.hashicorp.com/hc/en-us/requests/1580
 data "template_file" "user_data" {
   template = "${file("user_data.tpl")}"
 
